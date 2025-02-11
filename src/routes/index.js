dc2fc5b46733a5b4905c56693f44cf7f07b46627
Solaris5959 <connormmcdonald@gmail.com>
2025-02-10 21:50:48 -0500

    Fix commenting error

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,16 +9,13 @@ const router = express.Router();
 // Our authentication middleware
 const { authenticate } = require('../auth');
 const { createSuccessResponse } = require('../response');
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all with middleware so you have to be authenticated
  * in order to access things.
  */
 router.use(`/v1`, authenticate(), require('./api'));
-/**
- * Expose all of our API routes on /v1/* to include an API version.
- */
-router.use(`/v1`, require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
@@ -27,7 +24,6 @@ router.use(`/v1`, require('./api'));
 router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // return 200 'OK' response with info about the repo.
   res.status(200).json(
     createSuccessResponse({
       author,
